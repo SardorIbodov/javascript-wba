462fb561bb5b2cd1c2905a484a33e843bd540729
SardorIbodov <sardoribodov07@gmail.com>
2023-01-24 14:18:32 +0500

    JS | 1

--- js1/index.js
+++ js1/index.js
@@ -1,1 +1,44 @@
-console.log(typeof(1));
+// variables
+// text = "Sardor"; // it will be var, not let or const!!!
+
+// data types
+
+// primitive
+// number
+// (+-)9007199254740992 -> biggest value of the number type
+// console.log(9007199254740992 + 1);
+// console.log(9007199254740992n + 1n);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+
+// string -> '', "", ``
+// console.log("Welcome to \"Webbrain\" ");
+// let name = "Webbrain";
+// console.log("Welcome to 'Webbrain'");
+// console.log('Welcome to "Webbrain"');
+// console.log(`Welcome to ${name},
+// IT Center`);
+
+// typeof
+// console.log(typeof 1);
+
+
+// null vs undefined 
+// console.log(null === null); // t
+// console.log(undefined === undefined); // t
+// console.log(null == undefined); // t
+// console.log(null === undefined); // f
+
+
+// symbols -> for creating identification
+// let a = Symbol("text"),
+// 		b = Symbol("text");
+// console.log(a, b);
+// console.log(a == b);
+// console.log(a === b);
+
+// non-primitive
+// Object -> object, array, function
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof function hey(){});
