cab5ed075c0c05c86c3c76529e52c92800346ad2
SardorIbodov <sardoribodov07@gmail.com>
2023-01-24 10:59:08 +0500

    About Git and GitHub

--- github/review.js
+++ github/review.js
@@ -1,2 +1,21 @@
-// connection
+// config
+// git config --global user.name "userName"
+// git config --global user.email "userEmail"
+
+// connection 
 // git init
+// git remote add origin URL
+
+// push
+// git add .
+// git commit -m ""
+// git push origin master
+
+// branch
+// git branch
+// git branch newBranch
+// git checkout otherBranch
+
+// pull 
+// git pull origin otherBranch
+
