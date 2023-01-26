f6252035d8a3d3f8766f2b17084833c79a00f197
SardorIbodov <sardoribodov07@gmail.com>
2023-01-26 17:59:41 +0500

    pass by ref and value

--- js1/weird.js
+++ js1/weird.js
@@ -7,3 +7,23 @@
 // console.log(null === 0); // false
 // console.log(null > 0); // false
 // console.log(null >= 0); // true
+
+// Pass by reference
+// let obj = {
+// 	a: 1
+// }
+// function change(object) {
+// 	object.a = 100;
+// }
+// console.log(obj);
+// change(obj);
+// console.log(obj);
+
+// Pass by value
+// let a = 1;
+// function edit(num) {
+// 	return num * 2;
+// }
+// console.log(a);
+// console.log(edit(a));
+// console.log(a);
