da5e2e8411e1e4b83307f5b6f7a11130d3e46116
SardorIbodov <sardoribodov07@gmail.com>
2023-01-27 16:57:06 +0500

    JS | 2

---
+++ js2/index.js
@@ -0,0 +1,54 @@
+// Type convertions
+// console.log(1 + '1'); // "11"
+// console.log('Webbrain' + 'Academy'); // "WebbrainAcademy"
+// console.log(isNaN('1')); // false
+// console.log(isNaN('webbrain')); // true
+// console.log(1 + +'1'); // 2
+
+// console.log(Number('1.2')); // 1.2
+// console.log(Number.parseInt('1.2')); // 1
+// console.log(Number.parseFloat('1.2')); // 1.2
+// console.log(parseInt('1.2')); // 1
+// console.log(parseFloat('1.2')); // 1.2
+
+// console.log(false == 0); // true
+// console.log(true == 1); // true
+// console.log(null == 0); // false
+// console.log(null == false); // false
+// console.log(true == '0'); // false
+// console.log(true == '1'); // true
+// console.log(true == '2'); // false
+// console.log(true == 'a'); // true
+// console.log(false == 'a'); // false
+
+// console.log(Boolean(0)); // false
+// console.log(Boolean(null)); // false
+// console.log(Boolean(undefined)); // false
+// console.log(Boolean("")); // false
+// console.log(Boolean('')); // false
+// console.log(Boolean(``)); // false
+
+// console.log(Boolean(1)); // true
+// console.log(Boolean("0")); // true
+// console.log(Boolean("1")); // true
+// console.log(Boolean("a")); // true
+
+// Abbreviations
+// let a = 1000;
+// a += 10;
+// a -= 5;
+// a *= 4;
+// a /= 2;
+// console.log(a);
+
+// Comparision
+// console.log(10 > 11); // false
+// console.log(10 < 11); // true
+// console.log(10 >= 11); // false
+// console.log(10 <= 11); // true
+// console.log(10 == 11); // false
+// console.log(10 == 10); // true
+// console.log(10 === 10); // true
+// console.log(10 == '10'); // true
+// console.log(10 === '10'); // false
+// console.log('a' > 'A'); // true, because of ASCII table!!!
