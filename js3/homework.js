510361c41785fa98352cb4e9a34ae06441268701
SardorIbodov <sardoribodov07@gmail.com>
2023-01-31 12:09:03 +0500

    JS | 3

---
+++ js3/homework.js
@@ -0,0 +1,55 @@
+// 1
+// if ("0") {
+//   console.log("hello");
+// }
+// "0" -> true, answer: "hello"
+
+// 2
+// let value = -19;
+
+// if (value > 0) {
+// 	console.log(1);
+// } else if(value < 0) {
+// 	console.log(-1);
+// } else {
+// 	console.log(0);
+// }
+
+// 3
+// let nameOfJs = "ECMAScript";
+// if (nameOfJs === "ECMAScript") console.log("right");
+// else console.log("You don't know? ECMAScript");
+
+// 4
+// let a = 3, b = 2;
+// let result;
+
+// if (a + b < 4) {
+//   result = "below";
+// } else {
+// 	result = "over";
+// }
+
+// a + b < 4 ? result = "below" : result = "over";
+// console.log(result);
+
+// 5
+// let login = "Employee";
+// let message;
+
+// if (login === "Employee") {
+//   message = "Hello";
+// } else if (login === "Director") {
+//   message = "Greetings";
+// } else if (login === "") {
+//   message = "No login";
+// } else {
+// 	message = "";
+// }
+
+// login === "Employee" ? message = "Hello"
+// : login === "Director" ? message = "Greetings" 
+// : login === "" ? message = "No login" 
+// : message = "";
+
+// console.log(message);
