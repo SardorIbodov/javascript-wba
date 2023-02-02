b593ffd529e904dfa344a09e4b14a128b329e1df
SardorIbodov <sardoribodov07@gmail.com>
2023-02-02 17:21:36 +0500

    Switch-case / Loops

--- js4/class.js
+++ js4/class.js
@@ -3,11 +3,53 @@
 // switch-case
 // let weather = "snowy";
 // switch(weather) {
-// 	case "rainny": console.log("It's rainny today"); 
+// 	case "rainny": console.log("It's rainny today");
 // 	break;
 // 	case "sunny": console.log("It's sunny today");
 // 	break;
 // 	case "snowy": console.log("It's snowy today");
 // 	break;
 // 	default: console.log("We don't have any ideas!");
+// }
+
+// for loop
+// let i = 10;
+
+// for(; i >= 1; i--) { // 1-parametrni tushurib qoldirish (kod ishlayveradi)
+// 	console.log(i);
+// }
+
+// for(; ; i--) { // 1- va 2-parametrlarni tushirib qoldirish (cheksiz siklga tushib qoladi)
+// 	console.log(i);
+// }
+
+// for(; i >= 1;) { // 1- va 3-parametrlarni tushirib qoldirish (cheksiz siklga tushib qoladi)
+// 	console.log(i);
+// }
+
+// while loop
+// let i = 3;
+// while(i > 0) {
+// 	console.log(i);
+// 	i--;
+// }
+
+// do-while loop
+// let i = 0;
+// do {
+// 	console.log(i);
+// } while(i)
+
+// while(i) {
+// 	console.log(i);
+// }
+
+// break vs continue 
+// for(let i = 0; i <= 10; i++) {
+// 	if(i === 5) continue;
+// 	console.log(i);
+// }
+// for(let i = 0; i <= 10; i++) {
+// 	console.log(i);
+// 	if(i === 5) break;
 // }
