086c9440b5f9c11137959fc1e0dff68ce5971e52
SardorIbodov <sardoribodov07@gmail.com>
2023-02-02 16:33:39 +0500

    Switch-case / Loops

---
+++ js4/class.js
@@ -0,0 +1,13 @@
+// if (true) (console.log(1), console.log(2), console.log(3)); // if ni ichida bir necha qator kod yozish
+
+// switch-case
+// let weather = "snowy";
+// switch(weather) {
+// 	case "rainny": console.log("It's rainny today"); 
+// 	break;
+// 	case "sunny": console.log("It's sunny today");
+// 	break;
+// 	case "snowy": console.log("It's snowy today");
+// 	break;
+// 	default: console.log("We don't have any ideas!");
+// }
