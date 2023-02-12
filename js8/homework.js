a48a7518856a47ecae2c500a99c7e9df3f43bf42
SardorIbodov <sardoribodov07@gmail.com>
2023-02-12 12:45:55 +0500

    String

--- js8/homework.js
+++ js8/homework.js
@@ -1,7 +1,7 @@
-// check(str1, str2)
-// 1
+// check(str1, str2);
+// 1;
 // const check = (str1, str2) => str1.localeCompare(str2);
-// 2
+// 2;
 // const check = (str1, str2) =>
 //   str1 === str2
 //     ? `mos`
@@ -9,11 +9,11 @@
 //     ? `qisman mos`
 //     : `umaman mos emas`;
 
-// getCount()
+// getCount();
 // let getCount = (str) => {
 //   letters = "abcdefghijklmnopqrstuvwxyz";
 //   let result = {};
-// 	str = str.toLowerCase();
+//   str = str.toLowerCase();
 //   for (let char of str) {
 //     if (letters.includes(char))
 //       result[char] ? result[char]++ : (result[char] = 1);
@@ -21,24 +21,26 @@
 //   return result;
 // };
 
-// truncate()
-// const truncate = (str, count) => count >= 0 ? str.slice(0, count) : `musbat son kiriting!`;
+// truncate();
+// const truncate = (str, count) =>
+//   count >= 0 ? str.slice(0, count) : `musbat son kiriting!`;
 
-// getCurrency
+// getCurrency;
 // const getCurrency = (money) => money.slice(1);
 
-// calcCount()
+// calcCount();
 // const calcCount = (str) => {
 //   base = {
 //     letters: "abcdefghijklmnopqrstuvwxyz",
 //     numbers: "1234567890",
-// 		characters: "~`!@#$%^&*()-_=+/\|]}[{';\":><.,"
+//     characters: "~`!@#$%^&*()-_=+/|]}[{';\":><.,",
 //   };
-// 	result = {};
-// 	for(char of str) {
-// 		for(let key in base) {
-// 			if(base[key].includes(char)) result[key] ? result[key]++ : result[key] = 1;
-// 		}
-// 	}
-// 	return result;
+//   result = {};
+//   for (char of str) {
+//     for (let key in base) {
+//       if (base[key].includes(char))
+//         result[key] ? result[key]++ : (result[key] = 1);
+//     }
+//   }
+//   return result;
 // };
