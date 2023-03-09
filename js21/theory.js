f6112553a734822d682c4dbc872aebe65a7baa59
SardorIbodov <sardoribodov07@gmail.com>
2023-03-09 13:49:15 +0500

    __proto__ | prototype

---
+++ js21/theory.js
@@ -0,0 +1,18 @@
+// let user1 = { name: "webbrain" };
+// let user2 = { title: "academy" };
+// Object.assign(user1, user2);
+// user1.__proto__ = user2;
+// console.log(user1.title);
+
+// let user = { name: "webbrain", last: "academy" };
+// Object.prototype.fullName = function () {
+//   console.log(this.name, this.last);
+// };
+// user.fullName();
+
+// let arr = [1, 2, 3, 4, 5];
+// Array.prototype.remove = function(n) {
+// 	let res = this.filter(v => v !== n);
+// 	console.log(res);
+// }
+// arr.remove(2);
