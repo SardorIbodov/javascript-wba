7b09219378e9826f6478e6ddd30dba2bcd892e18
SardorIbodov <sardoribodov07@gmail.com>
2023-03-10 10:22:25 +0500

    __proto__ | prototype

---
+++ js21/homework.js
@@ -0,0 +1,16 @@
+// let user = {
+//   name: "wba",
+//   title: "academy",
+//   founded: 2019,
+//   branches: 5,
+// };
+// Object.defineProperty(user, "name", {"writable": false})
+// user.name = "webbrain";
+// console.log(user);
+
+// remove(key)
+// Object.prototype.remove = function (key) {
+//   delete this[key];
+//   return this;
+// };
+// console.log(user.remove("branches"));
