e91157f20302e400df28738e6ff78b0c812b7ca4
SardorIbodov <sardoribodov07@gmail.com>
2023-03-17 11:14:35 +0500

    Curry function

---
+++ js27/theory.js
@@ -0,0 +1,36 @@
+// var name = "Sardor";
+// function get() {
+// 	console.log(this);
+// }
+// get();
+
+// var name = "Sardor";
+// const object = {
+//   age: 19,
+//   get() {
+//     console.log(this);
+//   },
+// };
+// object.get();
+
+// const get = (params) => {
+// 	while(params > 0) {
+// 		get(--params)
+// 	} 
+// }
+// get(10);
+
+//! Curry function => which takes few parameters and returns another function
+// function some(st1) {
+// 	return function(st2) {
+// 		return function(st3) {
+// 			return function(st4) {
+// 				return `${st1}${st2}${st3}${st4}`
+// 			}
+// 		}
+// 	}
+// }
+// console.log(some("Assalomu")(" alaykum")(" aziz")(" o'quvchilar"));
+ 
+
+
